814cae730706d0f8a25bd695d793817a8b065e7f
waraihisui <w.kookaburr@gmail.com>
2018-07-10 20:31:07 +0900

    ready-for-cafe added

--- kitchen.rb
+++ kitchen.rb
@@ -4,101 +4,58 @@ require 'cgi'
 require 'csv'
 require './def'
 cgi = CGI.new
-print cgi.header("text/html; charset=utf-8")
-print "<html><body>"
-def plus_each_ele(filename_to_write, *arr)
-    temp = read_file_line(filename_to_write)
-    f=open(filename_to_write, "w:UTF-8")
-        temp.each_with_index{ |item, i| f.write((item.to_i + arr[i].to_i).to_s+"\n")}
-    f.close
-end
-
-def zeroize(filename)
-    f=open(filename, "w:UTF-8")
-        11.times do
-            f.write("0\n")
-        end
-    f.close
-end
 begin
-    tableform = ""
+    print cgi.header("text/html; charset=utf-8")
+    print "<html><body>"
+
+    served_lines = cgi.params["served"] #提供済の行番号を取得、1行目はヘッダ
     kitchen_data = CSV.read('kitchen.csv', headers: true, encoding: "utf-8")
+    #ファイル消去
+    f=open('kitchen.csv','w:utf-8')
+    f.close
+    tableform = ""
+    register_data = CSV.read('register.csv', encoding: "utf-8")
     kitchen_data.each_with_index do |line, i|
-        line.each do |key, value|
-            case
-                #最初
-                when key == "desk_num" then print <<-EOS
-                    <div style="display:table-cell; border:1px solid #666">
-                        <table border="1">
-                            <tr><th>卓番号</th><th>#{line["desk_num"][value]}</th></tr>
-                    EOS
-                #最後
-                when key == "NumOfPeople" then print <<-EOS
-                            <tr> <td>提供済み</td> <td><input type="checkbox" name="served" value="#{line["desk_num"][i]}"></td> </tr>
-                        </table>
-                    </div>
-                    EOS
-                #真ん中
-                when value != "0" then print "<tr><td>#{key}</td><td>#{value}</td></tr>" unless value == nil
+        if i == 0 || served_lines.include?(i) == false then
+            f=CSV.open('kitchen.csv', 'a+:UTF-8')
+                f.puts line
+            f.close
+
+            line.each do |key, value|
+                case
+                    #最初
+                    when key == "desk_num" then print <<-EOS
+                        <div style="display:table-cell; border:1px solid #666">
+                            <table border="1">
+                                <tr><th>卓番号</th><th>#{line["desk_num"][value]}</th></tr>
+                        EOS
+                    #最後
+                    when key == "NumOfPeople" then print <<-EOS
+                                <tr> <td>提供済み</td> <td><input type="checkbox" name="served" value="#{line["desk_num"][i]}"></td> </tr>
+                            </table>
+                        </div>
+                        EOS
+                    #真ん中
+                    when value != "0" then print "<tr><td>#{key}</td><td>#{value}</td></tr>" unless value == nil
+                end
+            end
+        else
+            line.each_with_index do |ele, j|
+                register_data[line[0].to_i][j-1].to_i += ele.to_i unless i == 0
             end
         end
     end
-=begin
-        tableform += <<-EOS
-            <div style="display:table-cell; border:1px solid #666">
-                <table border="1">
-                    <tr><th>席番号</th><th>#{kitchen_data[0]["desk_num"]}</th></tr>
-        EOS
-        line.each do |key, value|
-            if value != "0" && key != "desk_num" then
-                tableform += <<-EOS
-                    <tr><td>#{key}</td><td>#{value}</td></tr>
-                EOS
-            end
-        end
-        tableform += <<-EOS
-                    <tr> <td>提供済み</td> <td><input type="checkbox" name="served" value="#{kitchen_data[0]["desk_num"]}"></td> </tr>
-                </table>
-            </div>
-        EOS
-    end
+    f=CSV.open('register.csv', 'a+:UTF-8')
+        f.puts register_data
+    f.close
 
-served = cgi.params["served"]
-
-desk_nums_serving = read_file_line("serving")
-#desk_num_serivingから提供済みのものをserving無しに移してservingから削除する準備
-served.each{ |desk_num_served|
-    temp = read_file_line("#{desk_num_served}_serving")
-    plus_each_ele(desk_num_served, *temp)
-    zeroize("#{desk_num_served}_serving")
-    desk_nums_serving.delete(desk_num_served)
-}
-f=open("serving", "w:UTF-8")
-    desk_nums_serving.each{ |item|
-        f.write("#{item}\n")#実際に削除(除いたもので上書き)
-    }
-f.close
-
-menu_name = read_file_line("MenuName.txt")
-tableform = ""
-desk_nums_serving.each{ |desk_num|
-    quantity = read_file_line("#{desk_num}_serving")
-    quantity.delete_at(0)#「人数」を除外
-
-    tableform << "<div style=\"display:table-cell; border:1px solid #666\"><table border=\"1\"><tr><th>席番号</th><th>#{desk_num}</th></tr>"
-    quantity.each_with_index{ |item, i|
-        tableform << "<tr><td>#{menu_name[i]}</td><td>#{item}</td></tr>"
-    }
-    tableform << "<tr><td>提供済み</td><td><input type=\"checkbox\" name=\"served\" value=\"#{desk_num}\"></td></tr></table></div>"
-}
-=end
 rescue
     error_cgi
 end 
 print <<EOF
     <form name="update" action="./kitchen.rb" method="post">
         <div style="display:table; ">
-        #{tableform}
+            #{tableform}
         </div>
         <input type="submit" value="更新">
     </form>
---
+++ ready-for-cafe-complete.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require 'cgi'
+require 'csv'
+require './def'
+cgi = CGI.new
+print cgi.header("text/html; charset=utf-8")
+
+begin
+num_of_desk = cgi["num_of_desk"]
+names=cgi.params["menu"]
+names.delete("")
+prices=cgi.params["price"]
+prices.delete("")
+
+f=CSV.open('MenuData.csv', 'w:UTF-8')
+    f.puts names
+    f.puts prices
+f.close
+
+kitchen_data = ["desk_of_num"]
+kitchen_data += names
+kitchen_data += ["memo", "NumOfPeople"]
+f=CSV.open('kitchen.csv', 'w:UTF-8')
+    f.puts kitchen_data
+f.close
+
+register_data = Array.new()
+names.size.times do
+    register_data << "0" 
+end
+register_data += ["memo", "NumOfPeople"]
+f=CSV.open('register.csv', 'w:UTF-8')
+    num_of_desk.to_i.times do
+        f.puts register_data
+    end
+f.close
+rescue
+    error_cgi
+end
+print <<-EOF
+<html><body>
+    <a href="./kitchen.rb">kitchen</a>
+</body></html>
+EOF
