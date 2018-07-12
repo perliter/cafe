6285ea3398392c4dcf7be5b31fae418faccd9f7c
waraihisui <w.kookaburr@gmail.com>
2018-07-12 21:26:36 +0900

    kitchen.rb become clear

--- kitchen.rb
+++ kitchen.rb
@@ -2,65 +2,59 @@
 # encoding: utf-8
 require 'cgi'
 require 'csv'
-require 'matrix'
 require './def'
 cgi = CGI.new
 begin
-    print cgi.header("text/html; charset=utf-8")
-    print "<html><body>"
-
-    served_lines = cgi.params["served"] #提供済の行番号を取得
     kitchen_data = CSV.read('kitchen.csv', encoding: "utf-8")
-    menu_data = CSV.read('MenuData.csv', encoding: "utf-8")
-    menu_name = menu_data[0].to_a + ["memo"]
-    #ヘッダを書いてファイル初期化
-    f=CSV.open('kitchen.csv','w:utf-8')
-#        f.puts menu_name
-    f.close
-#    kitchen_data.delete_at(0)
-
-    tableform = ""
     register_data = CSV.read('register.csv', encoding: "utf-8")
-    kitchen_data.each_with_index do |line, i|
-        if served_lines.include?(i.to_s) == false then
-            f=CSV.open('kitchen.csv', 'a+:UTF-8')
+    menu_name = CSV.read('MenuData.csv', encoding: "utf-8")[0].to_a + ["memo"]
+#    menu_name = menu_data[0].to_a + ["memo"]
+ 
+    tableform = ""#html書き込み用
+    f=CSV.open('kitchen.csv','w:utf-8')
+        kitchen_data.each_with_index do |line, i|
+            if cgi.params["served"].include?(i.to_s) then
+            #処理する行が提供済み注文行と一致していれば、既存注文(なければ全要素0)に提供し終えたものを加算
+                #卓番号を基準にした＆要素数合わせ
+                desk_num = line[0].to_i - 1; line.delete_at(0)
+
+                #要素足し合わせ、足したいものが数値かを正規表現で判定して数値ならto_i
+                line.each_with_index do |value, j|
+                    if value =~ /^[0-9]+$/ then
+                        value = value.to_i
+                        register_data[desk_num][j] = register_data[desk_num][j].to_i
+                    end
+                    register_data[desk_num][j] += value
+                end
+            else
+            #不一致なら再書き込み＆表示の準備
                 f.puts line.to_a
-            f.close
             
-            line.each_with_index do |value, j|
-                case j
-                    #最初
-                    when 0 then tableform += <<-EOS
-                        <div style="display:table-cell; border:1px solid #666">
-                            <table border="1">
-                                <tr><th>卓番号</th><th>#{value}</th></tr>
-                        EOS
-                    #最後
-                    when line.size-1 then tableform += <<-EOS
-                                <tr> <td>提供済み</td> <td><input type="checkbox" name="served" value="#{i}"></td> </tr>
-                            </table>
-                        </div>
-                        EOS
-                    #真ん中
-                    else
-                        value = 0 if value == nil 
-                        tableform += "<tr><td>#{menu_name[j-1]}</td><td>#{value}</td></tr>" unless value.to_i == 0
+                line.each_with_index do |value, j|
+                    if (value != "" && value != "0") || j == line.size-1 then
+                    #人数以外のvalueが空じゃないならそれに応じてhtml生成
+                        tableform += case j
+                            #最初(卓番号)
+                            when 0; <<-EOS
+                                <div style="display:table-cell; border:1px solid #666">
+                                    <table border="1">
+                                        <tr><th>卓番号</th><th>#{value}</th></tr>
+                                EOS
+                            #最後(提供済み)
+                            when line.size-1; <<-EOS
+                                        <tr> <td>提供済み</td> <td><input type="checkbox" name="served" value="#{i}"></td> </tr>
+                                    </table>
+                                </div>
+                                EOS
+                            #真ん中(注文とmemo)
+                            else "<tr><td>#{menu_name[j-1]}</td><td>#{value}</td></tr>" 
+                        end
+                    end
                 end
-            end
-        else
-            #既存注文(なければ全要素0)に追加注文されて提供し終えたものを加算
-            line_num = line[0].to_i - 1
-            line.delete_at(0)
-#            register_data[line_num] = (Vector[*register_data[line_num]] + Vector[*line.to_a]).to_a
-            line.each_with_index do |value, j|
-                if value =~ /^[0-9]+$/ then
-                    value = value.to_i
-                    register_data[line_num][j] = register_data[line_num][j].to_i
-                end
-                register_data[line_num][j] += value
             end
         end
-    end
+    f.close
+    #提供済みのものが加算された結果をregister.csvに書き込み
     f=CSV.open('register.csv', 'w:UTF-8')
         register_data.each do |row|
             f.puts row
@@ -69,14 +63,16 @@ begin
 rescue
     error_cgi
 end 
+print cgi.header("text/html; charset=utf-8")
 print <<EOF
+<html><body>
     <form name="update" action="./kitchen.rb" method="post">
         <div style="display:table; ">
             #{tableform}
         </div>
         <input type="submit" value="更新">
     </form>
-    </body></html>
+</body></html>
 EOF
 
  
